fbe2c39d855303fdf9fd635f5a8dbe26a7ffca92
zenit <alexandrage@users.noreply.github.com>
2020-01-05 01:52:34 +0300

    up

--- src/head/Main.java
+++ src/head/Main.java
@@ -4,7 +4,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	public void onEnable() {
-		new Packet(this).hack();
 		new Scheduler(this).start();
 	}
 }
--- src/head/Scheduler.java
+++ src/head/Scheduler.java
@@ -3,31 +3,29 @@ package head;
 import org.bukkit.Bukkit;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.wrappers.WrappedChatComponent;
 public class Scheduler extends Thread {
 	Main plugin;
+
 	Scheduler(Main plugin) {
 		this.plugin = plugin;
 	}
 
 	@Override
 	public void run() {
-		for(;;) {
-			for(Player p : Bukkit.getOnlinePlayers()) {
-				try {
-			        PacketContainer chat = new PacketContainer(PacketType.Play.Server.CHAT);
-			        chat.getBytes().write(0, (byte)2);
-			        chat.getChatComponents().write(0, WrappedChatComponent.fromJson("{\"text\": \"§4§l❤§r "+(int)p.getHealth()+"/"+p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue()+"\"}"));
-			        ProtocolLibrary.getProtocolManager().sendServerPacket(p, chat);
-				} catch(Exception e) {}
+		for (;;) {
+			for (Player p : Bukkit.getOnlinePlayers()) {
+				p.setHealthScaled(true);
+				p.spigot().sendMessage(ChatMessageType.ACTION_BAR,
+						TextComponent.fromLegacyText("§4§l❤§r " + (int) p.getHealth() + "/"
+								+ (int) p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue()));
 			}
 			try {
 				Thread.sleep(500);
-			} catch (Exception e) {}
+			} catch (Exception e) {
+			}
 		}
 	}
 }
